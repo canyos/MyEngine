5956104f3b35a2f79ff7c3410f75ea4592cdcba8
canyos <4581974@naver.com>
2024-10-17 03:16:50 +0900

    삼각형 그리기

--- MyEngin_Source/pGraphicDevice.cpp
+++ MyEngin_Source/pGraphicDevice.cpp
@@ -1,5 +1,6 @@
 #include "pGraphicDevice.h"
 #include "pApplication.h"
+#include "pRenderer.h"
 
 extern p::Application application;
 
@@ -14,6 +15,33 @@ namespace p::graphics {
 	{
 		FLOAT backgroundColor[4] = { 0.2f, 0.2f, 0.2f, 1.0f };
 		mContext->ClearRenderTargetView(mRenderTargetView.Get(), backgroundColor);
+		//depth stencil view 초기화
+		mContext->ClearDepthStencilView(mDepthStencilView.Get(), D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.f, 0);
+		
+		//뷰포트 만들어주기
+		D3D11_VIEWPORT viewPort =
+		{
+			0, 0, application.GetWidth(), application.GetHeight(),
+			0.0f, 1.0f
+		};
+
+		//render target한개만 사용해서 묶어주기
+		mContext->RSSetViewports(1, &viewPort);
+		mContext->OMSetRenderTargets(1, mRenderTargetView.GetAddressOf(), mDepthStencilView.Get());
+		
+		//어떤 방식으로 연결해서 그려줄거냐
+		mContext->IASetInputLayout(renderer::inputLayouts);
+		mContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY::D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+		
+		UINT vertexSize = sizeof(renderer::Vertex);
+		UINT offset = 0;
+		mContext->IASetVertexBuffers(0, 1, &renderer::vertexBuffer, &vertexSize, &offset);
+		
+		mContext->VSSetShader(renderer::vsShader, 0, 0);
+		mContext->PSSetShader(renderer::psShader, 0, 0);
+
+		//파이프라인 그려줌
+		mContext->Draw(3, 0);
 
 		mSwapChain->Present(1, 0);
 
@@ -93,6 +121,62 @@ namespace p::graphics {
 			return;
 		if (FAILED(mDevice->CreateDepthStencilView(mDepthStencil.Get(), nullptr, mDepthStencilView.GetAddressOf())))
 			return;
+
+		DWORD shaderFlags = D3DCOMPILE_ENABLE_STRICTNESS;
+		shaderFlags |= D3DCOMPILE_DEBUG;
+		shaderFlags |= D3DCOMPILE_SKIP_OPTIMIZATION;
+		//vertex shader
+		{
+			ID3DBlob* errorBlob = nullptr;
+			D3DCompileFromFile(L"..\\Shaders_SOURCE\\TriangleVS.hlsl", nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE
+				, "main", "vs_5_0", shaderFlags, 0, &renderer::vsBlob, &errorBlob);
+			mDevice->CreateVertexShader(renderer::vsBlob->GetBufferPointer()
+				, renderer::vsBlob->GetBufferSize(), nullptr, &renderer::vsShader);
+		}
+		//pixel shader
+		{
+			ID3DBlob* errorBlob = nullptr;
+			D3DCompileFromFile(L"..\\Shaders_SOURCE\\TrianglePS.hlsl", nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE
+				, "main", "ps_5_0", shaderFlags, 0, &renderer::psBlob, &errorBlob);
+			mDevice->CreatePixelShader(renderer::psBlob->GetBufferPointer()
+				, renderer::psBlob->GetBufferSize(), nullptr, &renderer::psShader);
+		}
+		D3D11_INPUT_ELEMENT_DESC inputLayoutDesces[2] = {};
+		inputLayoutDesces[0].AlignedByteOffset = 0;//어디서부터 시작할지
+		inputLayoutDesces[0].Format = DXGI_FORMAT_R32G32B32_FLOAT;//3개
+		//x,y,z, r,g,b,a 중 x,y,z를 읽어라
+		inputLayoutDesces[0].InputSlot = 0;
+		inputLayoutDesces[0].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+		inputLayoutDesces[0].SemanticName = "POSITION";//position에 매칭
+		inputLayoutDesces[0].SemanticIndex = 0;
+		inputLayoutDesces[1].AlignedByteOffset = 12;//rgba를 읽어라
+		inputLayoutDesces[1].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+		inputLayoutDesces[1].InputSlot = 0;
+		inputLayoutDesces[1].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+		inputLayoutDesces[1].SemanticName = "COLOR";
+		inputLayoutDesces[1].SemanticIndex = 0;
+		mDevice->CreateInputLayout(inputLayoutDesces, 2
+			, renderer::vsBlob->GetBufferPointer()
+			, renderer::vsBlob->GetBufferSize()
+			, &renderer::inputLayouts);
+
+		D3D11_BUFFER_DESC bufferDesc = {};
+		bufferDesc.ByteWidth = sizeof(renderer::Vertex) * 3;
+		bufferDesc.BindFlags = D3D11_BIND_FLAG::D3D11_BIND_VERTEX_BUFFER;
+		bufferDesc.Usage = D3D11_USAGE::D3D11_USAGE_DYNAMIC;
+		bufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_FLAG::D3D11_CPU_ACCESS_WRITE;
+
+		//xyz
+		//rgba
+		renderer::vertexes[0].pos = Vector3(0.f, 0.5f, 0.0f);
+		renderer::vertexes[0].color = Vector4(0.0f, 1.0f, 0.0f, 1.0f);
+		renderer::vertexes[1].pos = Vector3(0.5f, -0.5f, 0.0f);
+		renderer::vertexes[1].color = Vector4(1.0f, 0.0f, 0.0f, 1.0f);
+		renderer::vertexes[2].pos = Vector3(-0.5f, -0.5f, 0.0f);
+		renderer::vertexes[2].color = Vector4(0.0f, 0.0f, 1.0f, 1.0f);
+		D3D11_SUBRESOURCE_DATA sub = { renderer::vertexes };
+		//sub.pSysMem = renderer::vertexes;
+		mDevice->CreateBuffer(&bufferDesc, &sub, &renderer::vertexBuffer);
 
 		int a = 0;
 	}
--- MyEngin_Source/pRenderer.cpp
+++ MyEngin_Source/pRenderer.cpp
@@ -1,7 +1,14 @@
 #include "pRenderer.h"
 #include "pCamera.h"
-namespace p {
-	namespace renderer {
-		Camera* mainCamera = nullptr;
-	}
+namespace p::renderer{
+	Camera* mainCamera = nullptr;
+
+	Vertex vertexes[3] = {};
+	ID3D11Buffer* vertexBuffer = nullptr; //vectex를 gpu로 옮기기 위한 버퍼
+	ID3DBlob* vsBlob = nullptr; //코드 뭉터기
+	ID3D11VertexShader* vsShader = nullptr;//가공할 쉐이더
+	ID3DBlob* psBlob = nullptr;
+	ID3D11PixelShader* psShader = nullptr;
+	ID3D11InputLayout* inputLayouts = nullptr;
+
 }
